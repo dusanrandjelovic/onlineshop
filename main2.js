7b2c4169e592f662453b7c1fa8db1f1993f997de
Dusan Randjelovic <dusanrandjelovic92@gmail.com>
2019-02-27 12:47:23 +0100

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,6 +1,9 @@
 const ispisivanjeUkupno = document.getElementById("iznos");
+const naruceno = document.getElementById("naruceno");
+const ukupnacena = document.getElementById("ukupnacena");
 
 ispisivanjeUkupno.innerText = localStorage.nesto;
+ukupnacena.value = localStorage.nesto;
 
 const prostor1n = document.getElementById("ispis1n");
 const prostor2n = document.getElementById("ispis2n");
@@ -11,3 +14,4 @@ prostor1n.innerHTML = localStorage.novo;
 prostor2n.innerHTML = localStorage.drugo;
 prostor3n.innerHTML = localStorage.trece;
 prostor4n.innerHTML = localStorage.cetvrto;
+naruceno.value = localStorage.novo + localStorage.drugo + localStorage.trece + localStorage.cetvrto;
